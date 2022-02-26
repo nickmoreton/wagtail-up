8fa06f91ec73498e1881d97d296af82a5899ed7c
Nick Moreton <nickmoreton@me.com>
2022-02-26 21:09:36 +0000

    initial test

---
+++ app/tests/test_home_page.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from wagtail.tests.utils import WagtailPageTests
+
+from app.home.models import HomePage
+
+
+class MyPageTests(WagtailPageTests):
+    # There's only one page model, so we can only test one page type
+    def test_can_create_home_page(self):
+        self.assertCanCreateAt(HomePage, HomePage)
